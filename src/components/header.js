c772836b966344221070e18aae1a336e57f1a638
Gabi Purcaru <gabi@purcaru.com>
2020-12-12 18:41:18 +0000

    add sections

--- src/components/header.js
+++ src/components/header.js
@@ -4,12 +4,27 @@ function Header() {
   return (
     <header className="text-gray-700 body-font bg-green-200">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" href="https://gabi.purcaru.com/">
+        <a className="flex title-font font-medium items-center text-gray-800 mb-4 md:mb-0" href="https://gabi.purcaru.com/">
           <span className="ml-3 text-2xl">Gabi Purcaru</span>
         </a>
-        <span className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center justify-center text-2xl text-gray-600">
+        <span className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center justify-center text-2xl text-gray-700">
           Software Engineer
         </span>
+        <span className="md:flex-1"></span>
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4	flex flex-wrap items-center text-base justify-center">
+          <a className="mr-6 text-gray-800 hover:text-black">
+            <svg className="w-5 h-5 inline md:mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"></path></svg>
+            Contact
+           </a>
+          <a className="mr-6 text-gray-800 hover:text-black">
+            <svg className="w-5 h-5 inline md:mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z" /></svg>
+            Blog
+          </a>
+          <a className="mr-6 text-gray-800 hover:text-black">
+            <svg className="w-5 h-5 inline md:mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" /></svg>
+            LinkedIn
+          </a>
+        </nav>
       </div>
     </header>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,10 @@ function IndexPage() {
         title="Home"
       />
 
-      <p className="leading-relaxed text-xl">I&apos;m a passionate software engineer that takes pride in his work. Currently, I&apos;m fighting fake accounts and platform abuse on Facebook and Instagram. Here are some of my personal projects.</p>
+      <p className="leading-relaxed text-2xl md:px-16 -mb-8 text-center">
+        I&apos;m a passionate software engineer that takes pride in his work.
+        Currently, I&apos;m fighting fake accounts and platform abuse on Facebook and Instagram.
+        Here&apos;s a bit about me.</p>
 
       <Experience />
       <Projects />
@@ -29,8 +32,8 @@ function Projects() {
             <div className="h-full bg-gray-100 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
               <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">Mule Uploader</h1>
               <p className="leading-relaxed mb-3">HTML5 resumable uploader for Amazon S3.
-It slices the files in JS, and then it
-uploads them piece by piece. At the end,
+              It slices the files in JS, and then it
+              uploads them piece by piece. At the end,
 S3 recreates the file from the pieces.</p>
               <a className="text-green-500 inline-flex items-center">Learn More
                 <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2} fill="none" strokeLinecap="round" strokeLinejoin="round">
@@ -44,8 +47,8 @@ S3 recreates the file from the pieces.</p>
             <div className="h-full bg-gray-100 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
               <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">Dedo.io</h1>
               <p className="leading-relaxed mb-3">Fancy calculator with unit and currency support.
-A mix between spreadsheets, notepad and a bit of
-Wolfram Alpha.<br/>Implemented with React, Material Design and CodeMirror.
+              A mix between spreadsheets, notepad and a bit of
+Wolfram Alpha.<br />Implemented with React, Material Design and CodeMirror.
 It features a mini programming language implemented
 in JavaScript with JISON.</p>
               <a className="text-green-500 inline-flex items-center">Learn More
@@ -60,7 +63,7 @@ in JavaScript with JISON.</p>
             <div className="h-full bg-gray-100 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
               <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">Acceleratul.ro</h1>
               <p className="leading-relaxed mb-3">Search platform for the Romanian railways.
-Built with React + ES6, Firebase, Material Design
+              Built with React + ES6, Firebase, Material Design
 and ServiceWorker.</p>
               <a className="text-green-500 inline-flex items-center">Learn More
                 <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2} fill="none" strokeLinecap="round" strokeLinejoin="round">
@@ -77,62 +80,104 @@ and ServiceWorker.</p>
 }
 
 function Experience() {
-    return (
-      <section className="text-gray-700 body-font overflow-hidden">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="-my-8">
-            <div className="py-8 flex flex-wrap md:flex-no-wrap">
-              <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span className="tracking-widest font-medium title-font text-gray-900">CATEGORY</span>
-                <span className="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
-              </div>
-              <div className="md:flex-grow">
-                <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">Bitters hashtag waistcoat fashion axe chia unicorn</h2>
-                <p className="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
-                <a className="text-indigo-500 inline-flex items-center mt-4">Learn More
-                  <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2} fill="none" strokeLinecap="round" strokeLinejoin="round">
-                    <path d="M5 12h14" />
-                    <path d="M12 5l7 7-7 7" />
-                  </svg>
-                </a>
-              </div>
+  return (
+    <section className="text-gray-700 body-font overflow-hidden">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="-my-8">
+          <div className="py-8 flex flex-wrap md:flex-no-wrap">
+            <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+              <span className="tracking-widest font-medium title-font text-gray-900">Facebook London</span>
+              <span className="mt-1 text-gray-500 text-sm">March 2017 &ndash; today</span>
             </div>
-            <div className="py-8 flex border-t-2 border-gray-200 flex-wrap md:flex-no-wrap">
-              <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span className="tracking-widest font-medium title-font text-gray-900">CATEGORY</span>
-                <span className="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
-              </div>
-              <div className="md:flex-grow">
-                <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">Meditation bushwick direct trade taxidermy shaman</h2>
-                <p className="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
-                <a className="text-indigo-500 inline-flex items-center mt-4">Learn More
-                  <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2} fill="none" strokeLinecap="round" strokeLinejoin="round">
-                    <path d="M5 12h14" />
-                    <path d="M12 5l7 7-7 7" />
-                  </svg>
-                </a>
-              </div>
-            </div>
-            <div className="py-8 flex border-t-2 border-gray-200 flex-wrap md:flex-no-wrap">
-              <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span className="tracking-widest font-medium title-font text-gray-900">CATEGORY</span>
-                <span className="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
-              </div>
-              <div className="md:flex-grow">
-                <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">Woke master cleanse drinking vinegar salvia</h2>
-                <p className="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
-                <a className="text-indigo-500 inline-flex items-center mt-4">Learn More
-                  <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2} fill="none" strokeLinecap="round" strokeLinejoin="round">
-                    <path d="M5 12h14" />
-                    <path d="M12 5l7 7-7 7" />
-                  </svg>
-                </a>
-              </div>
+            <div className="md:flex-grow">
+              <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">Software Engineer</h2>
+              <p className="leading-relaxed">
+                I&apos;ve revolved around the human review, security, and integrity/platform abuse spaces.
+                Along with excellent colleagues, I&apos;ve created a SQL-like DSL for online/offline security event analysis,
+                aligned Instagram&apos;s account-level abuse detection with the Facebook equivalent.
+                I&apos;ve led my organisation in implementing emerging data use regulations on a tight deadline,
+                and helped define the future paradigm of human review.</p>
             </div>
           </div>
+          <div className="py-8 flex border-t-2 border-gray-200 flex-wrap md:flex-no-wrap">
+            <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+              <span className="tracking-widest font-medium title-font text-gray-900">Facebook London</span>
+              <span className="mt-1 text-gray-500 text-sm">September &ndash; November 2016</span>
+            </div>
+            <div className="md:flex-grow">
+              <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">Software Engineer Intern</h2>
+              <p className="leading-relaxed">I&apos;ve helped improve Facebook&apos;s machine learning dataset labeling platform.</p>
+            </div>
+          </div>
+          <div className="py-8 flex border-t-2 border-gray-200 flex-wrap md:flex-no-wrap">
+            <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+              <span className="tracking-widest font-medium title-font text-gray-900">Facebook London</span>
+              <span className="mt-1 text-gray-500 text-sm">September &ndash; November 2015</span>
+            </div>
+            <div className="md:flex-grow">
+              <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">Software Engineer Intern</h2>
+              <p className="leading-relaxed"></p>
+              <a className="text-indigo-500 inline-flex items-center mt-4">Learn More
+                  <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2} fill="none" strokeLinecap="round" strokeLinejoin="round">
+                  <path d="M5 12h14" />
+                  <path d="M12 5l7 7-7 7" />
+                </svg>
+              </a>
+            </div>
+          </div>
+          <div className="py-8 flex border-t-2 border-gray-200 flex-wrap md:flex-no-wrap">
+            <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+              <span className="tracking-widest font-medium title-font text-gray-900">CATEGORY</span>
+              <span className="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
+            </div>
+            <div className="md:flex-grow">
+              <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">Woke master cleanse drinking vinegar salvia</h2>
+              <p className="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
+              <a className="text-indigo-500 inline-flex items-center mt-4">Learn More
+                  <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2} fill="none" strokeLinecap="round" strokeLinejoin="round">
+                  <path d="M5 12h14" />
+                  <path d="M12 5l7 7-7 7" />
+                </svg>
+              </a>
+            </div>
+          </div>
+          <div className="py-8 flex border-t-2 border-gray-200 flex-wrap md:flex-no-wrap">
+            <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+              <span className="tracking-widest font-medium title-font text-gray-900">CATEGORY</span>
+              <span className="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
+            </div>
+            <div className="md:flex-grow">
+              <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">Woke master cleanse drinking vinegar salvia</h2>
+              <p className="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
+              <a className="text-indigo-500 inline-flex items-center mt-4">Learn More
+                  <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2} fill="none" strokeLinecap="round" strokeLinejoin="round">
+                  <path d="M5 12h14" />
+                  <path d="M12 5l7 7-7 7" />
+                </svg>
+              </a>
+            </div>
+          </div>
+          <div className="py-8 flex border-t-2 border-gray-200 flex-wrap md:flex-no-wrap">
+            <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+              <span className="tracking-widest font-medium title-font text-gray-900">CATEGORY</span>
+              <span className="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
+            </div>
+            <div className="md:flex-grow">
+              <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">Woke master cleanse drinking vinegar salvia</h2>
+              <p className="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
+              <a className="text-indigo-500 inline-flex items-center mt-4">Learn More
+                  <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2} fill="none" strokeLinecap="round" strokeLinejoin="round">
+                  <path d="M5 12h14" />
+                  <path d="M12 5l7 7-7 7" />
+                </svg>
+              </a>
+            </div>
+          </div>
+
         </div>
-      </section>
-    );
+      </div>
+    </section>
+  );
 }
 
 export default IndexPage;
