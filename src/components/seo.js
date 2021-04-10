8e6c7d399674ba4f21cf0e346bae0823e5483dd0
Gabi Purcaru <gabi@purcaru.com>
2021-04-10 20:00:25 +0100

    fix query error in dev mode

--- src/components/seo.js
+++ src/components/seo.js
@@ -4,7 +4,8 @@ import React from "react";
 import { Helmet } from "react-helmet";
 
 function SEO({ description, lang, meta, keywords, title }) {
-  const { site } = useStaticQuery(graphql`
+  try {
+    const { site } = useStaticQuery(graphql`
     query DefaultSEOQuery {
       site {
         siteMetadata {
@@ -16,60 +17,65 @@ function SEO({ description, lang, meta, keywords, title }) {
     }
   `);
 
-  const metaDescription = description || site.siteMetadata.description;
+    const metaDescription = description || site.siteMetadata.description;
 
-  return (
-    <Helmet
-      htmlAttributes={{
-        lang,
-      }}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ]
-        .concat(
-          keywords.length > 0
-            ? {
+    return (
+      <Helmet
+        htmlAttributes={{
+          lang,
+        }}
+        meta={[
+          {
+            name: `description`,
+            content: metaDescription,
+          },
+          {
+            property: `og:title`,
+            content: title,
+          },
+          {
+            property: `og:description`,
+            content: metaDescription,
+          },
+          {
+            property: `og:type`,
+            content: `website`,
+          },
+          {
+            name: `twitter:card`,
+            content: `summary`,
+          },
+          {
+            name: `twitter:creator`,
+            content: site.siteMetadata.author,
+          },
+          {
+            name: `twitter:title`,
+            content: title,
+          },
+          {
+            name: `twitter:description`,
+            content: metaDescription,
+          },
+        ]
+          .concat(
+            keywords.length > 0
+              ? {
                 name: `keywords`,
                 content: keywords.join(`, `),
               }
-            : []
-        )
-        .concat(meta)}
-      title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
-    />
-  );
+              : []
+          )
+          .concat(meta)}
+        title={title}
+        titleTemplate={`%s | ${site.siteMetadata.title}`}
+      />
+    );
+  } catch (e) {
+    // sometimes this happens in dev mode for some reason...
+    console.log('Error fetching SEO metadata: ', e);
+    return null;
+  }
 }
 
 SEO.defaultProps = {
